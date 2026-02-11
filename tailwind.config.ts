d833b0647700de9e20ed0f788e5db98ec5e64b46
Tamij <mohammed.tamijuddin@ambrotechs.com>
2026-02-11 19:54:30 +0530

    Uploaded the Font Exosquad

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,7 +104,7 @@ export default {
 			fontFamily: {
 				'poppins': ['Poppins', 'sans-serif'],
 				'orbitron': ['Orbitron', 'sans-serif'],
-				'exosquad': ['"Exo 2"', 'sans-serif'],
+				'exosquad': ['Exosquad', '"Exo 2"', 'sans-serif'],
 			}
 		}
 	},
